ddfe700707958191fac96c9737181289c87f0796
VedantGupta19402 <vedantguptav@gmail.com>
2026-04-21 23:05:34 +0530

    Refactor: flatten project structure (ink → root)

---
+++ src/pages/Archive.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Section4 from '../components/Section4/Section4'
+import PageTransition from '../components/PageTransition'
+
+const Archive = () => {
+  return (
+    <>
+      <Section4 />
+      <PageTransition />
+    </>
+  )
+}
+
+export default Archive
